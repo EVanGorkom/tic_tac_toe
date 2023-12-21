5051a667ccca81e5d8315b0d3c7156e8e50dd983
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-12-20 21:37:59 -0600

    Add CSS styling to the basic functions of the board

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -2,15 +2,15 @@ import Tile from "./Tile"
 import Strike from "./Strike"
 
 function Board() {
-  return <div>
-    <Tile />
-    <Tile />
-    <Tile />
-    <Tile />
-    <Tile />
-    <Tile />
-    <Tile />
-    <Tile />
+  return <div className="board">
+    <Tile className="right-border bottom-border"/>
+    <Tile className="right-border bottom-border"/>
+    <Tile className="bottom-border"/>
+    <Tile className="right-border bottom-border"/>
+    <Tile className="right-border bottom-border"/>
+    <Tile className="bottom-border"/>
+    <Tile className="right-border"/>
+    <Tile className="right-border"/>
     <Tile />
     <Strike />
   </div>;
